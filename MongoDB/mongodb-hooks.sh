57270de7620dd9f8912f1eaa4fcb260e6ba1ecf9
Patric Uebele <Patric.Uebele@gmail.com>
2024-09-16 18:05:12 +0200

    Fix an issue in testing the inputs, replace mongo with mongosh.

--- MongoDB/mongodb-hooks.sh
+++ MongoDB/mongodb-hooks.sh
@@ -2,7 +2,7 @@
 #
 #
 # Pre and post execution hooks for MongoDB (non-sharded)
-# This execution hook has been tested with MongoDB 5.0.8 (deployed using Bitnami Helm chart) and NetApp Astra Control Service 22.04
+# This execution hook has been tested with MongoDB 7.0.12 (deployed using Bitnami Helm chart) and NetApp Astra Control Service 22.04
 # args: [pre|post]
 # pre: Performs a lock operation of the MongoDB instance using db.fsyncLock(). This flushes all write operations to the PVC and prevents additional writes by locking MongoDB.
 # post: Unlock MongoDB instance. Runs db.fsyncUnlock()
@@ -54,7 +54,7 @@ check_input(){
 
   export ACTION=$1
 
-  if [ $ACTION != "post" && $ACTION != "pre" ]; then
+  if [ "${ACTION}" != "post" ] && [ "${ACTION}" != "pre" ]; then
     error "$0: Usage: $0 [pre|post]"
     exit ${eusage}
   fi
@@ -63,14 +63,16 @@ check_input(){
 # Run prehook steps here
 #
 prehook() {
-    mongo admin --eval "printjson(db.fsyncLock())" --authenticationDatabase admin -u root -p $MONGODB_ROOT_PASSWORD
+    # mongo admin --eval "printjson(db.fsyncLock())" --authenticationDatabase admin -u root -p $MONGODB_ROOT_PASSWORD
+    mongosh admin --eval "printjson(db.fsyncLock())" --authenticationDatabase admin -u root -p $MONGODB_ROOT_PASSWORD
     return 0
 }
 #
 # Run posthook steps here
 #
 posthook() {
-    mongo admin --eval "printjson(db.fsyncUnlock())" --authenticationDatabase admin -u root -p $MONGODB_ROOT_PASSWORD
+    # mongo admin --eval "printjson(db.fsyncUnlock())" --authenticationDatabase admin -u root -p $MONGODB_ROOT_PASSWORD
+    mongosh admin --eval "printjson(db.fsyncUnlock())" --authenticationDatabase admin -u root -p $MONGODB_ROOT_PASSWORD
     return 0
 }
 #
